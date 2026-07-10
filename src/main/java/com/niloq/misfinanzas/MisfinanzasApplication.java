716d9d9bec32e7d1b2477008dabcf31d94c6ba68
Nilo <nilo_2804@hotmail.com>
2026-07-10 18:03:11 -0500

    cambio 1.0

--- src/main/java/com/niloq/misfinanzas/MisfinanzasApplication.java
+++ src/main/java/com/niloq/misfinanzas/MisfinanzasApplication.java
@@ -2,8 +2,9 @@ package com.niloq.misfinanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableAsync
 @EnableScheduling //NOTIFICACIONES
 @SpringBootApplication
 public class MisfinanzasApplication {
--- src/main/java/com/niloq/misfinanzas/service/EmailService.java
+++ src/main/java/com/niloq/misfinanzas/service/EmailService.java
@@ -3,6 +3,7 @@ package com.niloq.misfinanzas.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 //import jakarta.annotation.PostConstruct;
@@ -14,9 +15,11 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    
     @Value("${spring.mail.properties.mail.smtp.from}")
     private String fromEmail;
 
+    @Async   
     public void sendEmail(String to, String subjet, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
@@ -27,8 +30,7 @@ public class EmailService {
             mailSender.send(message);
 
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
+            System.err.println("FALLO AL ENVIAR CORREO: " + e.getMessage());
         }
 
     }
